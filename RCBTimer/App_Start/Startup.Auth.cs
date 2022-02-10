e2a260cb4f5a90ecddde8dc22753efff24b7366b
Guillermo Oramas <guillermo.oramas.1@gmail.com>
2022-02-10 13:16:22 -0500

    dummy changes

--- RCBTimer/App_Start/Startup.Auth.cs
+++ RCBTimer/App_Start/Startup.Auth.cs
@@ -26,12 +26,13 @@ namespace RCBTimer
             {
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
                 LoginPath = new PathString("/Account/Login"),
+                ExpireTimeSpan = TimeSpan.FromHours(72),
                 Provider = new CookieAuthenticationProvider
                 {
                     // Enables the application to validate the security stamp when the user logs in.
                     // This is a security feature which is used when you change a password or add an external login to your account.  
                     OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
-                        validateInterval: TimeSpan.FromMinutes(30),
+                        validateInterval: TimeSpan.FromHours(72),
                         regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
                 }
             });            
